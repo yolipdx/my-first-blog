779142dad63ae04fd2a9eede8b5ae33974bef26f
yoli <yoli@pdx.edu>
2017-12-07 15:57:09 -0800

    connect post model and view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    """ post_list view """
-    return render(request, 'blog/post_list.html')
+    """ post_list view 
+        This is exactly what views are supposed to do: connect models and templates
+    """
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
